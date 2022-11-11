c942d9754250db6f5bad3b980cb3b49757a20a1f
roxas <jeropastrana@gmail.com>
2022-11-11 17:30:58 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime la frase y hago magia ");
+        try {
+
+            ArrayList<String> palabras = cambiarLetra(sc.nextLine());
+            String frase = reconstruir(palabras);
+            System.out.println(frase);
+        } catch (IOException e) {
+            System.out.println("Algo se ha ido a la mierda");
+        }
+    }
+
+    private static ArrayList<String> cambiarLetra(String cambiarLetra) throws IOException {
+        Process process = crearProceso("LetraCambiada.jar");
+
+        BufferedReader br = getBufferedReader(process);
+
+        PrintStream ps = getPrintStream(process);
+
+        ArrayList<String> palabras = new ArrayList<>();
+
+        ps.println(cambiarLetra);
+        ps.flush();
+
+        String palabra;
+        while (!(palabra = br.readLine()).equalsIgnoreCase("FIN")) {
+            palabras.add(palabra);
+        }
+
+        return palabras;
+    }
+
+    private static String reconstruir(ArrayList<String> palabras) throws IOException {
+        Process process = crearProceso("Construir.jar");
+
+        BufferedReader br = getBufferedReader(process);
+        PrintStream ps = getPrintStream(process);
+
+        for (String palabra : palabras) {
+            ps.println(palabra);
+            ps.flush();
+        }
+        ps.println("FIN");
+        ps.flush();
+
+        return br.readLine();
+    }
+
+    private static PrintStream getPrintStream(Process process) {
+        OutputStream os = process.getOutputStream();
+        PrintStream ps = new PrintStream(os);
+        return ps;
+    }
+
+    private static BufferedReader getBufferedReader(Process process) {
+        InputStreamReader isr = new InputStreamReader(process.getInputStream());
+        BufferedReader br = new BufferedReader(isr);
+        return br;
+    }
+
+    private static Process crearProceso (String proceso) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", proceso);
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        return process;
     }
 }
